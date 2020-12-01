23890f920aecdcb0962eadcc2067485bafac6cb8
kazuto.masuda <masuda@biansha.com>
2020-12-01 18:05:10 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by kazuto masuda on 2020/12/01.
+//  Copyright © 2020 kmzstdbnsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
